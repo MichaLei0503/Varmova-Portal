9c7bf01cabf5cec60957bc7e12bebf4380cb1b55
MichaLei0503 <business@michael-leibiger.com>
2026-03-23 12:14:24 +0100

    Create action-state.ts

---
+++ src/lib/action-state.ts
@@ -0,0 +1,9 @@
+export type ActionState = {
+  success: boolean;
+  message?: string;
+  errors?: Record<string, string[]>;
+};
+
+export const initialActionState: ActionState = {
+  success: false,
+};
